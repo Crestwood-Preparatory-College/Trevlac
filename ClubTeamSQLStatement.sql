4cdf80eef4f417ed1a6fa06b58a254e4db2f163c
Adam M. Anklewicz <adam@anklewicz.com>
2016-04-11 10:34:30 -0400

    Data previously included both the Upper and Lower school. This limits it to just the Upper School

--- ClubTeamSQLStatement.sql
+++ ClubTeamSQLStatement.sql
@@ -1,3 +1,1 @@
-/* This document was created for Mishka to pull all the students from all clubs and teams. */
-
-Select a_club_team_detail.club_team_name, a_club_team_detail.student_number, s_master.common_given_name, s_master.common_last_name from a_club_team_detail LEFT JOIN s_master on a_club_team_detail.student_number=s_master.student_number
+Select a_club_team_detail.club_team_name, a_club_team_detail.student_number, s_master.common_given_name, s_master.common_last_name from a_club_team_detail LEFT JOIN s_master on a_club_team_detail.student_number=s_master.student_number WHERE s_master.status='Active' and a_club_team_detail.school='665266' and s_master.school='665266'
